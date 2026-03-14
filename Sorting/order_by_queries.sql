ad39e9f87884273330451be5845731afc6ef89c2
Indicatorrr <ashwinshetty0410@gmail.com>
2026-03-14 13:33:48 +0530

    Create order_by_queries.sql

---
+++ Sorting/order_by_queries.sql
@@ -0,0 +1,9 @@
+-- List prerequisite courses ordered by course id
+SELECT *
+FROM prereq
+ORDER BY course_id;
+
+-- Instructor details sorted by salary descending
+SELECT *
+FROM instructor
+ORDER BY salary DESC;
