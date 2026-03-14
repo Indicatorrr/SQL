000c45bbc6b66d166c7d568e108d5999cf179c29
Indicatorrr <ashwinshetty0410@gmail.com>
2026-03-14 13:15:48 +0530

    Create group_by_queries.sql

---
+++ Aggregation/group_by_queries.sql
@@ -0,0 +1,40 @@
+-- Departments with highest average salary
+SELECT dept_name
+FROM instructor
+GROUP BY dept_name
+HAVING AVG(salary) >= ALL (
+    SELECT AVG(salary)
+    FROM instructor
+    GROUP BY dept_name
+);
+
+-- Departments whose budget is less than average instructor salary
+SELECT dept_name
+FROM department
+WHERE budget <= ALL (
+    SELECT AVG(salary)
+    FROM instructor
+);
+
+-- Courses offered at most once in 2009
+SELECT course_id
+FROM section
+WHERE year=2009
+GROUP BY course_id
+HAVING COUNT(*) <= 1;
+
+-- Students who opted at least two CSE courses
+SELECT S.ID, S.name
+FROM student S
+JOIN takes T ON S.ID = T.ID
+JOIN course C ON T.course_id = C.course_id
+WHERE C.dept_name='CSE'
+GROUP BY S.ID,S.name
+HAVING COUNT(DISTINCT T.course_id) >= 2;
+
+-- Average instructor salary per department (>42000)
+SELECT dept_name,
+       AVG(salary) AS avg_salary
+FROM instructor
+GROUP BY dept_name
+HAVING AVG(salary) > 42000;
